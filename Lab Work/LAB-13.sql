6e345e9167a09b6f56485c7de4adebd030021c5a
tirtharamoliya <tirtharamoliya@gmail.com>
2026-07-22 21:51:37 +0530

    Add files via upload

---
+++ Lab Work/LAB-13.sql
@@ -0,0 +1,65 @@
+-- 1. Combine information from Student and Result table using cross join (Cartesian product).
+SELECT * FROM STUDENT_INFO CROSS JOIN RESULT
+SELECT * FROM RESULT
+
+-- 2. Perform inner join on Student and Result tables.
+SELECT * FROM STUDENT_INFO INNER JOIN RESULT ON STUDENT_INFO.RNO=RESULT.RNO
+-- 3. Perform the left outer join on Student and Result tables.
+SELECT * FROM STUDENT_INFO AS S LEFT OUTER JOIN RESULT AS R ON S.RNO=R.RNO
+-- 4. Perform the right outer join on Student and Result tables.
+SELECT * FROM STUDENT_INFO AS S RIGHT OUTER JOIN RESULT AS R ON S.RNO=R.RNO
+-- 5. Perform the full outer join on Student and Result tables.
+SELECT * FROM STUDENT_INFO AS S FULL JOIN RESULT AS R ON S.RNO=R.RNO
+-- 6. Display Rno, Name, Branch and SPI of all students.
+SELECT S.RNO,NAME,BRANCH,SPI FROM STUDENT_INFO AS S 
+LEFT OUTER JOIN RESULT AS R ON S.RNO=R.RNO
+-- 7. Display Rno, Name, Branch and SPI of CE branch students only.
+SELECT S.RNO,NAME,BRANCH,SPI FROM STUDENT_INFO AS S 
+LEFT OUTER JOIN RESULT AS R ON S.RNO=R.RNO WHERE BRANCH='CE'
+-- 8. Display Rno, Name, Branch and SPI of students other than EC branch.
+SELECT S.RNO,NAME,BRANCH,SPI FROM STUDENT_INFO AS S 
+LEFT OUTER JOIN RESULT AS R ON S.RNO=R.RNO WHERE BRANCH<>'EC'
+-- 9. Display Rno, Name and SPI of students whose SPI is greater than 8.
+SELECT S.RNO,NAME,SPI FROM STUDENT_INFO AS S 
+LEFT OUTER JOIN RESULT AS R ON S.RNO=R.RNO WHERE SPI>8
+-- 10. Display Rno, Name and Branch of students whose SPI is less than 8.
+SELECT S.RNO,NAME,BRANCH,SPI FROM STUDENT_INFO AS S 
+LEFT OUTER JOIN RESULT AS R ON S.RNO=R.RNO WHERE SPI<8
+-- 11. Display average result of each branch.
+SELECT AVG(SPI),BRANCH FROM STUDENT_INFO AS S JOIN RESULT AS R ON S.RNO=R.RNO GROUP BY BRANCH
+-- 12. Display average result of CE and ME branch.
+SELECT AVG(SPI),BRANCH FROM STUDENT_INFO AS S JOIN RESULT AS R ON S.RNO=R.RNO WHERE BRANCH='ME' OR BRANCH='CE' 
+GROUP BY BRANCH
+-- 13. Display maximum and minimum SPI of each branch.
+SELECT MAX(SPI),BRANCH FROM STUDENT_INFO AS S JOIN RESULT AS R ON S.RNO=R.RNO GROUP BY BRANCH
+SELECT MIN(SPI),BRANCH FROM STUDENT_INFO AS S JOIN RESULT AS R ON S.RNO=R.RNO GROUP BY BRANCH
+
+-- 14. Display branch-wise student count in descending order.
+SELECT COUNT(S.RNO),BRANCH FROM STUDENT_INFO AS S JOIN RESULT AS R ON S.RNO=R.RNO GROUP BY BRANCH
+ORDER BY COUNT(S.RNO) DESC
+
+-- 15. Display branch-wise total SPI of students.
+SELECT SUM(S.RNO),BRANCH FROM STUDENT_INFO AS S JOIN RESULT AS R ON S.RNO=R.RNO GROUP BY BRANCH
+
+-- 16. Display branch-wise number of students having SPI greater than 8.
+SELECT COUNT(S.RNO),BRANCH FROM STUDENT_INFO AS S JOIN RESULT AS R ON S.RNO=R.RNO WHERE SPI>8 GROUP BY BRANCH 
+-- 17. Display branch-wise number of students having SPI less than 8.
+SELECT COUNT(S.RNO),BRANCH FROM STUDENT_INFO AS S JOIN RESULT AS R ON S.RNO=R.RNO WHERE SPI<8 GROUP BY BRANCH 
+
+-- 18. Display branch-wise average SPI greater than 7.
+SELECT AVG(SPI),BRANCH FROM STUDENT_INFO AS S JOIN RESULT AS R ON S.RNO=R.RNO GROUP BY BRANCH HAVING AVG(SPI)>7
+
+-- 19. Display branches having more than 1 students.
+SELECT COUNT(S.RNO),BRANCH FROM STUDENT_INFO AS S JOIN RESULT AS R ON S.RNO=R.RNO GROUP BY BRANCH HAVING COUNT(S.RNO)>1
+
+-- 20. Display branches where maximum SPI is greater than 9.
+SELECT MAX(SPI),BRANCH FROM STUDENT_INFO AS S JOIN RESULT AS R ON S.RNO=R.RNO GROUP BY BRANCH HAVING MAX(SPI)>9
+
+-- 21. Display average result of each branch and sort them in ascending order by SPI.
+SELECT AVG(SPI),BRANCH FROM STUDENT_INFO AS S JOIN RESULT AS R ON S.RNO=R.RNO GROUP BY BRANCH ORDER BY AVG(SPI)
+
+-- 22. Display highest SPI from each branch and sort them in descending order.
+SELECT MAX(SPI),BRANCH FROM STUDENT_INFO AS S JOIN RESULT AS R ON S.RNO=R.RNO GROUP BY BRANCH ORDER BY MAX(SPI) DESC
+
+-- 25. Display branches where difference between max and min SPI is greater than 1.
+SELECT MAX(SPI)-MIN(SPI) FROM STUDENT_INFO AS S JOIN RESULT AS R ON S.RNO=R.RNO GROUP BY BRANCH HAVING MAX(SPI)-MIN(SPI)>1
